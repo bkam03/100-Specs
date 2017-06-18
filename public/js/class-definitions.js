59effff9d63e2c6488cb9913e1a4af8c2f0b249e
BK <bkam03@yahoo.com>
2017-06-17 15:01:05 -1000

    Door class added

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1114,6 +1114,11 @@ class Box {
  * @param {boolean} isOpen Whether the door is opened or closed
  */
 
+class Door {
+  constructor ( isOpen ){
+    this.isOpen = isOpen;
+  }
+}
 
 /**
  * Step 55
